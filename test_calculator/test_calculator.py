e51d738652844d27e5f0ca224725328d1546c055
Jim Mullen <jimmullen@Jims-MacBook-Pro.local>
2025-02-06 20:01:36 -0500

    Further corrected pytest.ini and added functions to calculator and test_calculator

---
+++ test_calculator/test_calculator.py
@@ -0,0 +1,19 @@
+'''Calculator test'''
+from calculator import add, subtract, multiply, divide 
+
+def test_add():
+    '''Test add function'''
+    assert add(2,2) == 4
+
+def test_subtract():
+    '''Test subtraction function'''
+    assert subtract(2,2) == 0
+
+def test_multiply():
+    '''Test multiply function'''
+    assert multiply(2,2) == 4
+
+def test_divide():
+    '''Test division function'''
+    assert divide(2,2) == 1
+
