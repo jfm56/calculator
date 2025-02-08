652f8fe56f17895cc81cff0f72ee4f7d38ea00d2
Jim Mullen <jimmullen@Jims-MacBook-Pro.local>
2025-02-07 21:11:42 -0500

    removed cache files from git that were being tracked, added thest_calculations to diable pylint unnecesay dunder call for invalad name

--- test_calculator/test_calculator.py
+++ test_calculator/test_calculator.py
@@ -1,18 +1,18 @@
-'''Calculator test'''
+"""Test to ensure calculator functions properly."""
 from calculator import add, subtract, multiply, divide
 
 def test_add():
-    '''Test add function'''
+    """Test to see if add function works."""
     assert add(2,2) == 4
 
 def test_subtract():
-    '''Test subtraction function'''
+    """Test to see if subtraction function works."""
     assert subtract(2,2) == 0
 
 def test_multiply():
-    '''Test multiply function'''
+    """Test to see if multiply function works."""
     assert multiply(2,2) == 4
 
 def test_divide():
-    '''Test division function'''
+    """Test to see if division function works."""
     assert divide(2,2) == 1
